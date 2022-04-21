7e813d1b520ae520b4b7e1bf7eeb5e123d0c1269
Kristina-01 <gribkova01@bk.ru>
2022-04-21 23:45:01 +0300

    5

---
+++ app/src/main/java/ru/mirea/gribkova/mireaproject1/PlayService.java
@@ -0,0 +1,37 @@
+package ru.mirea.gribkova.mireaproject1;
+
+import android.app.Service;
+import android.content.Intent;
+import android.media.MediaActionSound;
+import android.media.MediaPlayer;
+import android.os.IBinder;
+import android.provider.MediaStore;
+
+class PlayerService extends Service {
+    private MediaPlayer mediaPlayer;
+
+    public PlayerService() {
+    }
+
+    @Override
+    public IBinder onBind(Intent intent) {
+        // TODO: Return the communication channel to the service.
+        throw new UnsupportedOperationException("Not yet implemented");
+    }
+
+    @Override
+    public void onCreate(){
+      //  mediaPlayer = MediaPlayer.create(this, MusicPlayer.song);
+        mediaPlayer.setLooping(true);
+    }
+    @Override
+    public int onStartCommand(Intent intent, int flags, int startId){
+        mediaPlayer.start();
+        return START_STICKY;
+    }
+    @Override
+    public void onDestroy() {
+        mediaPlayer.stop();
+    }
+}
+
